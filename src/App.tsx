5ff12069aa7c0eac2d2a7449f4bb797198a38443
Sainathredy05 <sainathreddyakkala@gmail.com>
2026-04-09 20:49:29 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -6,10 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/hooks/useAuth";
 import SEOProvider from "@/components/SEOProvider";
-
-import Nirf from "./pages/Nirf";
-// ... inside your Routes
-<Route path="/nirf" element={<Nirf />} />
 
 // Import pages
 import Index from "./pages/Index";
@@ -26,24 +21,9 @@ import ComputerScience from "./pages/ComputerScience";
 import Electronics from "./pages/Electronics";
 import ExaminationCell from "./pages/ExaminationCell";
 import Library from "./pages/departments/Library";
-import History from "./pages/about/History";
-import VisionMission from "./pages/about/VisionMission";
-import PrincipalMessage from "./pages/about/PrincipalMessage";
+import Nirf from "./pages/Nirf"; // NIRF Page Import
 
-import CollegePledgeSong from "./pages/about/CollegePledgeSong";
-import TeachingStaff from "./pages/administration/TeachingStaff";
-import NonTeachingStaff from "./pages/administration/NonTeachingStaff";
-import Organogram from "./pages/administration/Organogram";
-import GoverningBody from "./pages/administration/GoverningBody";
-import AcademicCouncil from "./pages/administration/AcademicCouncil";
-import CPDC from "./pages/administration/CPDC";
-import ProgramStructure from "./pages/academics/ProgramStructure";
-import ProgrammesOffered from "./pages/academics/ProgrammesOffered";
-import CourseOutcomes from "./pages/academics/CourseOutcomes";
-import ProgramOutcomes from "./pages/academics/ProgramOutcomes";
-import IQAC from "./pages/iqac/IQAC";
-import QualityPolicy from "./pages/iqac/QualityPolicy";
-import StudentSupport from "./pages/StudentSupport";
+// ... keep all your other imports the same ...
 import Physics from "./pages/departments/Physics";
 import Chemistry from "./pages/departments/Chemistry";
 import Mathematics from "./pages/departments/Mathematics";
@@ -89,102 +69,102 @@ const App = () => (
           <Sonner />
           <BrowserRouter>
             <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/about-us" element={<AboutUs />} />
-            <Route path="/administration" element={<Administration />} />
-            <Route path="/academics" element={<Academics />} />
-            <Route path="/departments" element={<Departments />} />
-            <Route path="/departments/library" element={<Library />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/gallery" element={<Gallery />} />
-            <Route path="/departments/computerscience" element={<ComputerScience />} />
-            <Route path="/departments/electronics" element={<Electronics />} />
-            <Route path="/departments/physics" element={<Physics />} />
-            <Route path="/departments/chemistry" element={<Chemistry />} />
-            <Route path="/departments/mathematics" element={<Mathematics />} />
-            <Route path="/departments/commerce" element={<Commerce />} />
-            <Route path="/departments/english" element={<English />} />
-            <Route path="/departments/telugu" element={<Telugu />} />
-            <Route path="/departments/hindi" element={<Hindi />} />
-            <Route path="/departments/botany" element={<Botany />} />
-            <Route path="/departments/zoology" element={<Zoology />} />
-            <Route path="/departments/economics" element={<Economics />} />
-            <Route path="/departments/history" element={<DeptHistory />} />
-            <Route path="/departments/politicalscience" element={<PoliticalScience />} />
-            <Route path="/departments/physicaleducation" element={<PhysicalEducation />} />
-            <Route path="/examination-cell" element={<ExaminationCell />} />
-            <Route path="/library" element={<Library />} />
-            <Route path="/iqac" element={<IQAC />} />
-            <Route path="/iqac/quality-policy" element={<QualityPolicy />} />
-            <Route path="/student-support" element={<StudentSupport />} />
-            <Route path="/student-support/ncc-program" element={<NCCProgram />} />
-            <Route path="/student-support/women-empowerment" element={<WomenEmpowerment />} />
-            <Route path="/student-support/gym-sports" element={<GymSports />} />
-            <Route path="/placements" element={<Placements />} />
-            
-            {/* Admin Routes */}
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/admin/reset-password" element={<ResetPassword />} />
-            <Route path="/admin/accept-invitation" element={<AcceptInvitation />} />
-            <Route path="/admin/announcements" element={
-              <AdminRoleGuard>
-                <AdminLayout>
-                  <AnnouncementsAdmin />
-                </AdminLayout>
-              </AdminRoleGuard>
-            } />
-            <Route path="/admin/examination" element={
-              <AdminRoleGuard>
-                <AdminLayout>
-                  <ExaminationDocumentsAdmin />
-                </AdminLayout>
-              </AdminRoleGuard>
-            } />
-            <Route path="/admin/iqac" element={
-              <AdminRoleGuard>
-                <AdminLayout>
-                  <IqacAdmin />
-                </AdminLayout>
-              </AdminRoleGuard>
-            } />
-            <Route path="/admin/about-us" element={
-              <AdminRoleGuard>
-                <AdminLayout>
-                  <AicteAdmin />
-                </AdminLayout>
-              </AdminRoleGuard>
-            } />
-            <Route path="/admin/staff" element={
-              <AdminRoleGuard>
-                <AdminLayout>
-                  <StaffAdmin />
-                </AdminLayout>
-              </AdminRoleGuard>
-            } />
-            <Route path="/admin/gallery" element={
-              <AdminRoleGuard>
-                <AdminLayout>
-                  <GalleryAdmin />
-                </AdminLayout>
-              </AdminRoleGuard>
-            } />
-            <Route path="/admin/departments" element={
-              <AdminRoleGuard>
-                <AdminLayout>
-                  <DepartmentsAdmin />
-                </AdminLayout>
-              </AdminRoleGuard>
-            } />
-            <Route path="/admin/student-support" element={
-              <AdminRoleGuard>
-                <AdminLayout>
-                  <StudentSupportAdmin />
-                </AdminLayout>
-              </AdminRoleGuard>
-            } />
-            
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
+              <Route path="/" element={<Index />} />
+              <Route path="/about-us" element={<AboutUs />} />
+              <Route path="/administration" element={<Administration />} />
+              <Route path="/academics" element={<Academics />} />
+              <Route path="/departments" element={<Departments />} />
+              <Route path="/departments/library" element={<Library />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/gallery" element={<Gallery />} />
+              <Route path="/nirf" element={<Nirf />} /> {/* NIRF Route added here */}
+              <Route path="/departments/computerscience" element={<ComputerScience />} />
+              <Route path="/departments/electronics" element={<Electronics />} />
+              <Route path="/departments/physics" element={<Physics />} />
+              <Route path="/departments/chemistry" element={<Chemistry />} />
+              <Route path="/departments/mathematics" element={<Mathematics />} />
+              <Route path="/departments/commerce" element={<Commerce />} />
+              <Route path="/departments/english" element={<English />} />
+              <Route path="/departments/telugu" element={<Telugu />} />
+              <Route path="/departments/hindi" element={<Hindi />} />
+              <Route path="/departments/botany" element={<Botany />} />
+              <Route path="/departments/zoology" element={<Zoology />} />
+              <Route path="/departments/economics" element={<Economics />} />
+              <Route path="/departments/history" element={<DeptHistory />} />
+              <Route path="/departments/politicalscience" element={<PoliticalScience />} />
+              <Route path="/departments/physicaleducation" element={<PhysicalEducation />} />
+              <Route path="/examination-cell" element={<ExaminationCell />} />
+              <Route path="/library" element={<Library />} />
+              <Route path="/iqac" element={<IQAC />} />
+              <Route path="/iqac/quality-policy" element={<QualityPolicy />} />
+              <Route path="/student-support" element={<StudentSupport />} />
+              <Route path="/student-support/ncc-program" element={<NCCProgram />} />
+              <Route path="/student-support/women-empowerment" element={<WomenEmpowerment />} />
+              <Route path="/student-support/gym-sports" element={<GymSports />} />
+              <Route path="/placements" element={<Placements />} />
+              
+              {/* Admin Routes */}
+              <Route path="/admin" element={<Admin />} />
+              <Route path="/admin/reset-password" element={<ResetPassword />} />
+              <Route path="/admin/accept-invitation" element={<AcceptInvitation />} />
+              <Route path="/admin/announcements" element={
+                <AdminRoleGuard>
+                  <AdminLayout>
+                    <AnnouncementsAdmin />
+                  </AdminLayout>
+                </AdminRoleGuard>
+              } />
+              <Route path="/admin/examination" element={
+                <AdminRoleGuard>
+                  <AdminLayout>
+                    <ExaminationDocumentsAdmin />
+                  </AdminLayout>
+                </AdminRoleGuard>
+              } />
+              <Route path="/admin/iqac" element={
+                <AdminRoleGuard>
+                  <AdminLayout>
+                    <IqacAdmin />
+                  </AdminLayout>
+                </AdminRoleGuard>
+              } />
+              <Route path="/admin/about-us" element={
+                <AdminRoleGuard>
+                  <AdminLayout>
+                    <AicteAdmin />
+                  </AdminLayout>
+                </AdminRoleGuard>
+              } />
+              <Route path="/admin/staff" element={
+                <AdminRoleGuard>
+                  <AdminLayout>
+                    <StaffAdmin />
+                  </AdminLayout>
+                </AdminRoleGuard>
+              } />
+              <Route path="/admin/gallery" element={
+                <AdminRoleGuard>
+                  <AdminLayout>
+                    <GalleryAdmin />
+                  </AdminLayout>
+                </AdminRoleGuard>
+              } />
+              <Route path="/admin/departments" element={
+                <AdminRoleGuard>
+                  <AdminLayout>
+                    <DepartmentsAdmin />
+                  </AdminLayout>
+                </AdminRoleGuard>
+              } />
+              <Route path="/admin/student-support" element={
+                <AdminRoleGuard>
+                  <AdminLayout>
+                    <StudentSupportAdmin />
+                  </AdminLayout>
+                </AdminRoleGuard>
+              } />
+              
+              <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
         </AuthProvider>
